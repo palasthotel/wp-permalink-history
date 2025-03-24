d97311b609c1dfe90022d3170602c83fa7d1d648
Edward <edward.bock@palasthotel.de>
2025-03-24 16:00:46 +0100

    fix danamic property warning

--- public/classes/HistoryItem.php
+++ public/classes/HistoryItem.php
@@ -1,53 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: edward
- * Date: 2019-04-10
- * Time: 18:02
- */
 
 namespace Palasthotel\PermalinkHistory;
 
-
-/**
- * @property int|null id
- * @property int content_id
- * @property string content_type
- * @property string permalink
- */
 class HistoryItem {
 
-	/**
-	 * HistoryItem constructor.
-	 *
-	 * @param int $content_id
-	 * @param string $content_type
-	 * @param string $permalink
-	 * @param int|null $id
-	 */
-	private function __construct($content_id, $content_type, $permalink, $id) {
-		$this->id = $id;
-		$this->content_id = $content_id;
-		$this->content_type = $content_type;
-		$this->permalink = $permalink;
+	private function __construct(public int $content_id, public string $content_type, public string $permalink, public ?int $id = null) {
 	}
 
-	/**
-	 * @param int $content_id
-	 * @param string $content_type
-	 * @param string $permalink
-	 * @param int|null $id
-	 *
-	 * @return HistoryItem
-	 */
-	public static function build($content_id, $content_type, $permalink, $id = null){
+	public static function build(int $content_id, string $content_type, string $permalink, ?int $id = null){
 		return new HistoryItem($content_id, $content_type, $permalink, $id);
 	}
 
 	/**
 	 * @param object|null $resultObject
 	 *
-	 * @return HistoryItem
 	 */
 	public static function parse($resultObject){
 		return (!is_object($resultObject))?
