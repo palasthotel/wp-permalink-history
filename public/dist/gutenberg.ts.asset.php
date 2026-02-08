43d09b53a6d9d1f682197b2898ed1f84ac5e8b85
JanaME <jana.eggebrecht@palasthotel.de>
2026-02-08 23:20:18 +0100

    chore: transpile

--- public/dist/gutenberg.ts.asset.php
+++ public/dist/gutenberg.ts.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-plugins'), 'version' => '84fac6d7fafa983070ef');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-plugins'), 'version' => 'c9652fdc75b8dd9c20d8');
